1d33eeaeff1469905ba7444c2cd77461aa18f730
uvsq21918311 <elkadi96hotmail.com>
2020-03-03 17:11:47 +0100

    Add Test RobotStatic

---
+++ src/test/java/fr/uvsq/pglp_3/LSP/RobotStaticTest.java
@@ -0,0 +1,17 @@
+package fr.uvsq.pglp_3.LSP;
+
+import org.junit.Test;
+
+/**
+ * tests unitaires de la classe RobotStatic
+ */
+public class RobotStaticTest {
+	@Test
+	/**
+	 * test du constructeur
+	 */
+	public void test() {
+		@SuppressWarnings("unused")
+		RobotStatic rs = new RobotStatic(0,0);
+	}
+}
