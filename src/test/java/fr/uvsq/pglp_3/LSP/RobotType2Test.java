5b5f2c3143a92b451c68b20db2b06772cc2f3609
uvsq21918311 <elkadi96hotmail.com>
2020-03-03 17:13:19 +0100

    Add Test RobotTestType1 & RobotTestType1

---
+++ src/test/java/fr/uvsq/pglp_3/LSP/RobotType2Test.java
@@ -0,0 +1,29 @@
+package fr.uvsq.pglp_3.LSP;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * tests unitaires de la classe RobotType2
+ */
+public class RobotType2Test {
+	@Test
+	/**
+	 * teste le constructeur et le getter du robot
+	 */
+	public void test() {
+		Robot r = new RobotType2(4);
+		assertTrue(r.getPosition().x == r.getPosition().y && r.getPosition().y == 0);
+	}
+	@Test
+	/**
+	 * teste si le robot avance correctement
+	 */
+	public void testAvance() {
+		Robot r = new RobotType2(4);
+		r.tourne();
+		r.avance();
+		assertTrue(r.getPosition().x == 4 && r.getPosition().y == 0);
+	}
+}
