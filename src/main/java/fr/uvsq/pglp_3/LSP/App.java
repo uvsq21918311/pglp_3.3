f8e5bca76dcfad3a5e65609c282652d03060db40
uvsq21918311 <elkadi96hotmail.com>
2020-03-28 14:58:40 +0100

    Update pom.xml

--- src/main/java/fr/uvsq/pglp_3/LSP/App.java
+++ src/main/java/fr/uvsq/pglp_3/LSP/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Robot r = new RobotType1();
+		r.tourne();
+		r.avance();
     }
 }
